28d9392b7222c4df1203635b815527fda64ea920
Konstantin Seregin <kraken114@bk.ru>
2024-11-20 21:00:21 +0300

    Jinja2 #1: О шаблонизаторе, использование {{ }} в шаблонах

--- список_уроков.py
+++ список_уроков.py
@@ -1,2 +1,5 @@
 # Урок_1 https://vk.com/video/@selfedu_rus?z=video302513503_456239435%2Fpl_302513503_-2
 # Урок_2 https://vk.com/video302513503_456239436?ref_domain=yastatic.net
+
+
+# Jinja_1: https://vk.com/video/@selfedu_rus?z=video302513503_456239429%2Fpl_302513503_-2
