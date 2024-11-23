89f2684c5aee40587f99df4f577a801eeed8ad9f
Konstantin Seregin <kraken114@bk.ru>
2024-11-23 21:53:17 +0300

    Flask #5: Подключение внешних ресурсов и работа с формами(часть_1)

---
+++ Онлайн_курс/Урок_5/my_5.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+
+menu = [{"name": "Установка", "url": "install-flask"},
+        {"name": "Первое приложение", "url": "first-app"},
+        {"name": "Обратная связь", "url": "contact"},]
+
+
+@app.route("/")
+def index():
+    print(url_for('index'))  # выдает адрес обработчика
+    return render_template('index.html', menu=menu)
+
+
+@app.route("/about")
+def about():
+    print(url_for('about'))  # выдает адрес обработчика
+    return render_template('about.html', title="О сайте", menu=menu)
+
+
+@app.route("/profile/<path:username>")
+# @app.route("/profile/<path>") можно и так
+def profile(username):
+    return f"Пользователь: {username}"
+# в кавычках можно писать:
+# path: это путь
+# int: это целое число
+# float: это число с плавающей точкой
+
+
+# выдает адрес обработчика
+# with app.test_request_context():
+#     print(url_for('index'))
+#     print(url_for('about'))
+#     print(url_for('profile', username="Marcus"))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- список_уроков.py
+++ список_уроков.py
@@ -1,2 +1,5 @@
 # Урок_1 https://vk.com/video/@selfedu_rus?z=video302513503_456239435%2Fpl_302513503_-2
 # Урок_2 https://vk.com/video302513503_456239436?ref_domain=yastatic.net
+# Урок_3 https://rutube.ru/video/677328f4010c99fc483ac5fc5fce9951/
+# Урок_4 https://rutube.ru/video/80ddcd4740242a130954273558f0f8f6/
+# Урок_5 https://rutube.ru/video/c4661253ba3718dbb54e850b3c5769c8/
